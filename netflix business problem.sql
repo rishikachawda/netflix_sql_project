15d014d0a9f36d2bf25419aca917e71a7db8bfd3
Rishika Chawda <rishikachawda01@gmail.com>
2025-04-25 16:44:42 +0530

    Add files via upload

---
+++ netflix business problem.sql
@@ -0,0 +1,28 @@
+-- 15 Business Problems based on Netflix Database.
+
+1. Count the total number of Movies vs TV Shows.
+2. Find the most common rating for the movies and  the TV shows.
+3. List all the movies that released in a specific year (e.g., 2020).
+4. Find the top 5 countries with the most content on the Netflix.
+5. Identify the longest movie.
+6. Find the content that added in the last 5 years.
+7. Find all the movies/TV shows by director 'Rajiv Chilaka'.
+8. List all the TV shows which have more than 5 seasons.
+9. Count the number of content items in each genre.
+10.Find each year and the average numbers of content release in India on netflix and
+   return top 5 year with highest avg content release.
+11. List all the movies that are documentaries.
+12. Find all the content that is without a director.
+13. Find out the name of movies in which 'Salman Khan' is appeared as Actor in the last 10 years.
+14. Find the top 10 actors who have appeared in the highest number of movies that produced in India.
+15.Categorize the content based on the presence of the keywords 'kill' and 'violence' in 
+   the description field. Label content containing these keywords as 'Bad' and all other 
+   content as 'Good'. Count how many items fall into each category.
+
+
+
+
+
+
+
+
